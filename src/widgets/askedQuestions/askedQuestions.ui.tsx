c1c4fed81d39df1c3ff94694b2948694a691ff17
sartovAkhmed <akhmedturan@gmail.com>
2025-01-29 19:16:53 +0600

    feat: feature askedQuestion component

---
+++ src/widgets/askedQuestions/askedQuestions.ui.tsx
@@ -0,0 +1,9 @@
+import { Container, Typography } from "@mui/material";
+import { AskedQuestionsList } from "~features/askedQuestionsList";
+
+export const AskedQuestions: React.FC = () => {
+  return <Container className="max-w-[1440px] mb-[150px]">
+    <Typography variant="h2" className="text-4xl mb-4">Часто задаваемые вопросы</Typography>
+    <AskedQuestionsList />
+  </Container>
+};
---
+++ src/widgets/askedQuestions/index.ts
@@ -0,0 +1,1 @@
+export { AskedQuestions } from "./askedQuestions.ui";
