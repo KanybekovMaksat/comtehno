41af9aaf222c6cf9cb5995890542a42639b28ece
sartovAkhmed <akhmedturan@gmail.com>
2025-02-03 14:52:47 +0600

    feat: feature butger menu for mobile

--- src/features/navigation/ui/Navigate.tsx
+++ src/features/navigation/ui/Navigate.tsx
@@ -1,6 +1,19 @@
-import { AppBar, Stack, Toolbar, Typography } from "@mui/material";
+import { 
+  AppBar, 
+  Drawer, 
+  IconButton, 
+  List, 
+  ListItem, 
+  ListItemText, 
+  Stack, 
+  Toolbar, 
+  useMediaQuery, 
+  useTheme 
+} from "@mui/material";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { pathKeys } from "~shared/lib/react-router";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export const Navigate: React.FC = () => {
   interface LinkItem {
@@ -9,62 +22,63 @@ export const Navigate: React.FC = () => {
   }
 
   const listLink: LinkItem[] = [
-    {
-      title: "Приёмная комиссия 2025",
-      url: "/error"
-    },
-    {
-      title: "О колледже",
-      url: "/error"
-    },
-    {
-      title: "Специальности",
-      url: "/error"
-    },
-    {
-      title: "Абитуриентам",
-      url: "/error"
-    },
-    {
-      title: "Новости",
-      url: "/news"
-    },
-    {
-      title: "Мероприятия",
-      url: "/error"
-    },
-    {
-      title: "Подобрать программу",
-      url: "/error"
-    },
-    {
-      title: "Расписание",
-      url: "/error"
-    },
-    {
-      title: "Студентам",
-      url: "/error"
-    },
-    {
-      title: "Отзывы",
-      url: "/error"
-    },
+    { title: "Приёмная комиссия 2025", url: "/error" },
+    { title: "О колледже", url: "/error" },
+    { title: "Специальности", url: "/error" },
+    { title: "Абитуриентам", url: "/error" },
+    { title: "Новости", url: "/news" },
+    { title: "Мероприятия", url: "/error" },
+    { title: "Подобрать программу", url: "/error" },
+    { title: "Расписание", url: "/error" },
+    { title: "Студентам", url: "/error" },
+    { title: "Отзывы", url: "/error" },
   ];
 
-  return <AppBar position="static" className="bg-white shadow-none">
-    <Toolbar className="flex items-center justify-between px-0 py-2">
-      <Stack direction={"row"} flexWrap={"wrap"}
-        className="gap-2 items-center">
-        <Link to={pathKeys.home()} className="font-[Geologica] text-[#0F766E] font-bold text-2xl">КОМТЕХНО</Link>
-        {listLink.map((item: LinkItem, index: number) => (
-          <Link 
-            to={item.url}
-            key={index} 
-            className="font-[Geologica] hover:bg-[#0D9488] hover:text-white transition-all border-[#E4E4E7] text-base text-black font-light border p-2 rounded-full">
-              {item.title}
-          </Link>
-        ))}
-      </Stack>
-    </Toolbar>
-  </AppBar>;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  // Функция управления Drawer
+  const toggleDrawer = (state: boolean) => () => {
+    setOpen(state);
+  };
+
+  return (
+    <AppBar position="static" className="bg-white shadow-none">
+      <Toolbar className="flex items-center justify-between px-0 py-2">
+        <Stack sx={{justifyContent: isMobile && "space-between"}} direction="row" flexWrap="wrap" className="gap-2 items-center w-full">
+          <Link to={pathKeys.home()} className="font-[Geologica] text-[#0F766E] font-bold text-2xl">КОМТЕХНО</Link>
+          {isMobile ? (
+            <>
+              <IconButton edge="start" color="inherit" onClick={toggleDrawer(true)}>
+                <MenuIcon className="text-black" />
+              </IconButton>
+
+              <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+                <List className="w-64">
+                  {listLink.map((item, index) => (
+                    <ListItem button key={index} onClick={toggleDrawer(false)}>
+                      <Link to={item.url} className="text-black w-full block p-2">
+                        <ListItemText primary={item.title} />
+                      </Link>
+                    </ListItem>
+                  ))}
+                </List>
+              </Drawer>
+            </>
+          ) : (
+            listLink.map((item, index) => (
+              <Link 
+                to={item.url}
+                key={index} 
+                className="font-[Geologica] hover:bg-[#0D9488] hover:text-white transition-all border-[#E4E4E7] text-base text-black font-light border p-2 rounded-full">
+                  {item.title}
+              </Link>
+            ))
+          )}
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
 };
--- src/widgets/header/header.ui.tsx
+++ src/widgets/header/header.ui.tsx
@@ -1,10 +1,14 @@
-import { Container } from "@mui/material";
+import { Container, useMediaQuery } from "@mui/material";
 import { Navigate } from "~features/navigation";
 import { Network } from "~features/networking";
+import { useTheme } from "@mui/material/styles";
 
 export const Header: React.FC = () => {
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"))
+
   return <Container className="max-w-[1440px]">
-    <Network />
+    {!isMobile && <Network />}
     <Navigate />
   </Container>;
 };
