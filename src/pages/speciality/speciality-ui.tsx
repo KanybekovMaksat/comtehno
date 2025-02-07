b728983e9b51ecf84782acef896ade8ce2a10e92
esdf123 <ruslanoveldar350@gmail.com>
2025-02-07 15:04:39 +0600

    priceTag

--- src/pages/speciality/speciality-ui.tsx
+++ src/pages/speciality/speciality-ui.tsx
@@ -3,6 +3,7 @@ import { SpecialHero } from "~widgets/specialHero";
 import { TrainingProgram } from "~widgets/trainingProgram";
 import { ProjectsGrid } from "~widgets/projectCards";
 import { Resume } from "~widgets/resume/Resume-ui";
+import { PriceTag } from "~widgets/priceTag/priceTag.ui";
 
 export const SpecialityPage: React.FC = () => {
   return <Container className="max-w-[1440px] container">
@@ -10,5 +11,6 @@ export const SpecialityPage: React.FC = () => {
     <TrainingProgram />
     <ProjectsGrid/>
     <Resume/>
+    <PriceTag/>
   </Container>;
 };
