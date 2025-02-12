4460da7e6458e4c0060dea343639a3a272567f9d
ma1abakiev <malabakievramzan@gmail.com>
2025-02-12 19:48:23 +0600

    add i18n

---
+++ src/shared/lib/api/apiClient.tsx
@@ -0,0 +1,22 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+import { getCookie } from 'typescript-cookie'
+
+const API_URL = 'https://new.comtehno.kg/api/'
+
+function getCurrentLanguage() {
+  return getCookie('language') || 'ru' // Язык по умолчанию — 'ru'
+}
+
+// Создаем экземпляр Axios с настройками по умолчанию
+export const apiClient: AxiosInstance = axios.create({
+  baseURL: API_URL,
+})
+
+// Добавляем перехватчик, который добавляет язык ко всем запросам
+apiClient.interceptors.request.use((config: AxiosRequestConfig) => {
+  config.headers = {
+    ...config.headers,
+    'Accept-Language': getCurrentLanguage(),
+  }
+  return config
+})
