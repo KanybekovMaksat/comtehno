a41e99971b50899968ca9552e60d01534146d9f4
sartovAkhmed <akhmedturan@gmail.com>
2025-02-19 22:06:44 +0600

    fix: fixed all components

--- src/entities/contactCard/ContactCard.tsx
+++ src/entities/contactCard/ContactCard.tsx
@@ -11,17 +11,17 @@ interface ContactCardProps {
 
 export const ContactCard: FC<ContactCardProps> = ({ icon, text, link }) => {
   return (
-    <Link to={link}>
-      <Card className="w-[398px] flex justify-between bg-[#F4F4F5] r-sm:h-14 h-[80px] items-center p-3 shadow-md rounded-lg transition hover:shadow-lg">
+    <Link className="ml-0" to={link}>
+      <Card className="r-md:w-80 w-[398px] flex justify-between bg-[#F4F4F5] r-sm:h-14 h-[80px] items-center p-3 shadow-md rounded-lg transition hover:shadow-lg">
         <div className="flex items-center space-x-3">
           <div className="flex items-center justify-center min-w-[40px] min-h-[40px]">
             {icon}
           </div>
-          <CardContent className="p-0">
-            <p className="text-gray-800 text-xl font-normal">
+          <div>
+            <span className="text-gray-800 r-md:text-lg text-xl font-normal">
               {text}
-            </p>
-          </CardContent>
+            </span>
+          </div>
         </div>
         <ArrowIcon />
       </Card>
--- src/features/partnersList/ui/partnersList.tsx
+++ src/features/partnersList/ui/partnersList.tsx
@@ -1,39 +1,65 @@
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import TelegramIcon from '@mui/icons-material/Telegram';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import AssuredWorkloadIcon from '@mui/icons-material/AssuredWorkload';
-import GoogleIcon from '@mui/icons-material/Google';
-import GTranslateIcon from '@mui/icons-material/GTranslate';
-import ExtensionIcon from '@mui/icons-material/Extension';
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import AssuredWorkloadIcon from "@mui/icons-material/AssuredWorkload";
+import GoogleIcon from "@mui/icons-material/Google";
+import GTranslateIcon from "@mui/icons-material/GTranslate";
+import ExtensionIcon from "@mui/icons-material/Extension";
 
-import Marquee from 'react-fast-marquee';
-import { PartnersCard } from './PartnersCard';
+import Marquee from "react-fast-marquee";
+import { PartnersCard } from "./PartnersCard";
 
 export interface PartnerCard {
   icon: JSX.Element;
   nameLogo: string;
-};
+}
 
 export const PartnersList: React.FC = () => {
   const parnerLogoList: PartnerCard[] = [
-    { icon: <WhatsAppIcon className="text-5xl text-green" />, nameLogo: "WhatsApp"},
-    { icon: <TelegramIcon className="text-5xl text-blue" />, nameLogo: "Telegram"},
-    { icon: <YouTubeIcon className="text-5xl text-red" />, nameLogo: "YouTube" },
-    { icon: <AssuredWorkloadIcon className="text-5xl text-[#392fc1]" />, nameLogo: "Assured" },
-    { icon: <GoogleIcon className="text-5xl text-blue" />, nameLogo: "Google" },
-    { icon: <GTranslateIcon className="text-5xl" />, nameLogo: "GTranslate" },
-    { icon: <ExtensionIcon className="text-5xl" />, nameLogo: "Extension" }
+    {
+      icon: <WhatsAppIcon className="r-md:text-4xl text-5xl text-green" />,
+      nameLogo: "WhatsApp",
+    },
+    {
+      icon: <TelegramIcon className="r-md:text-4xl text-5xl text-blue" />,
+      nameLogo: "Telegram",
+    },
+    {
+      icon: <YouTubeIcon className="r-md:text-4xl text-5xl text-red" />,
+      nameLogo: "YouTube",
+    },
+    {
+      icon: (
+        <AssuredWorkloadIcon className="r-md:text-4xl text-5xl text-[#392fc1]" />
+      ),
+      nameLogo: "Assured",
+    },
+    {
+      icon: <GoogleIcon className="r-md:text-4xl text-5xl text-blue" />,
+      nameLogo: "Google",
+    },
+    {
+      icon: <GTranslateIcon className="r-md:text-4xl text-5xl" />,
+      nameLogo: "GTranslate",
+    },
+    {
+      icon: <ExtensionIcon className="r-md:text-4xl text-5xl" />,
+      nameLogo: "Extension",
+    },
   ];
 
-  return <Marquee
-    direction="left"
-    speed={100}
-    gradient={true}
-    className="r-sm:mb-20 mb-[150px] h-[200px] w-full">
-      <div className='flex gap-6 mr-6'>
+  return (
+    <Marquee
+      direction="left"
+      speed={100}
+      gradient={true}
+      className="r-md:mb-20 mb-[150px] r-md:h-20 h-[200px] w-full"
+    >
+      <div className="flex r-md:gap-4 r-md:mr-4 gap-6 mr-6">
         {parnerLogoList.map((partnerCard: PartnerCard, index: number) => (
           <PartnersCard key={index} {...partnerCard} />
         ))}
       </div>
-  </Marquee>
+    </Marquee>
+  );
 };
