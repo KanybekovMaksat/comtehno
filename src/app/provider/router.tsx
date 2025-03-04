3d460f7d22ef51c645695de2a49fc55c37ada458
ma1abakiev <malabakievramzan@gmail.com>
2025-03-04 13:10:42 +0600

    add: documents page

--- src/app/provider/router.tsx
+++ src/app/provider/router.tsx
@@ -2,36 +2,37 @@ import {
   RouterProvider,
   createBrowserRouter,
   useRouteError,
-} from "react-router-dom";
+} from 'react-router-dom'
 
-import { aboutPageRoute } from "~pages/about";
-import { errorPageRoute } from "~pages/error";
-import { homePageRoute } from "~pages/home";
-import { newsDetailsPageRoute, newsPageRoute } from "~pages/news";
-import { GenericLayout } from "~app/layout";
-import { EventPageRoute } from "~pages/event";
-import { EventDetailsPageRoute } from "~pages/EventDetailPage";
-import { specialityPageRoute } from "~pages/speciality";
-import { reviewDetailPageRoute, reviewsPageRoute } from "~pages/reviews";
+import { aboutPageRoute } from '~pages/about'
+import { errorPageRoute } from '~pages/error'
+import { homePageRoute } from '~pages/home'
+import { newsDetailsPageRoute, newsPageRoute } from '~pages/news'
+import { GenericLayout } from '~app/layout'
+import { EventPageRoute } from '~pages/event'
+import { EventDetailsPageRoute } from '~pages/EventDetailPage'
+import { specialityPageRoute } from '~pages/speciality'
+import { reviewDetailPageRoute, reviewsPageRoute } from '~pages/reviews'
+import { documentDetailsPageRoute, documentsPageRoute } from '~pages/documents'
 
 function BubbleError() {
-  const error = useRouteError();
+  const error = useRouteError()
   if (error instanceof Error) {
-    console.error("Route Error:", error.message);
+    console.error('Route Error:', error.message)
   } else {
-    console.error("Unknown Route Error:", error);
+    console.error('Unknown Route Error:', error)
   }
   return (
     <div className="text-center text-red-500">
       <h1>Ошибка!</h1>
       <p>Что-то пошло не так. Пожалуйста, попробуйте позже.</p>
     </div>
-  );
+  )
 }
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <GenericLayout />,
     errorElement: <BubbleError />,
     children: [
@@ -48,10 +49,12 @@ const router = createBrowserRouter([
       // News
       newsPageRoute,
       newsDetailsPageRoute,
+      documentsPageRoute,
+      documentDetailsPageRoute,
     ],
   },
-]);
+])
 
 export function BrowserRouter() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />
 }
---
+++ src/pages/documents/documents.ui.tsx
@@ -0,0 +1,70 @@
+import { Typography, Container, Box } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+import { documentQueries, documentTypes } from '~entities/document'
+
+export const DocumentsPage = () => {
+  const { data: documentData } = documentQueries.useGetDocuments()
+
+  return (
+    <Container maxWidth="lg" className="py-10 px-4">
+      <Typography
+        variant="h4"
+        className="font-bold text-center mb-8 text-gray-800"
+        style={{ fontSize: 'clamp(1.5rem, 5vw, 2.4rem)' }}
+      >
+        Об университете
+      </Typography>
+      <Box
+        display="flex"
+        flexDirection="column"
+        gap={2}
+        className="notion-like-embed embed code notion-like-embed  mb-20"
+      >
+        {documentData?.data.map(
+          (document: documentTypes.DocumentSchema, i: number) => (
+            <Box
+              key={i}
+              className="flex items-center rounded-[8px] p-[16px] border border-[#e0e0e0] transition-transform duration-200 hover:translate-y-[-4px] md:flex md:flex-col "
+            >
+              <Box
+                component="img"
+                src={document.photo}
+                alt={document.title}
+                className="w-[64px] h-[64px] rounded-[8px] mr-[16px] object-cover"
+              />
+              <Box className="md:flex md:flex-col md:items-center md:gap-3">
+                <Typography
+                  variant="h6"
+                  className="font-medium text-primary md:text-center"
+                >
+                  {document.title}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    color: 'text.secondary',
+                  }}
+                  className="text-center"
+                >
+                  {document.subtitle}
+                </Typography>
+
+                <RouterLink
+                  to={`/document/${document.slug}`}
+                  style={{
+                    fontSize: '0.875rem',
+                    fontWeight: '500',
+                    color: '#1976d2',
+                    textDecoration: 'none',
+                  }}
+                >
+                  Перейти
+                </RouterLink>
+              </Box>
+            </Box>
+          )
+        )}
+      </Box>
+    </Container>
+  )
+}
