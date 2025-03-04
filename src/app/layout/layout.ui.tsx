7474f81942de0bd22660ce24165905d951f336e7
sartovAkhmed <akhmedturan@gmail.com>
2025-03-04 23:31:19 +0600

    feat: feature home function

--- src/app/layout/layout.ui.tsx
+++ src/app/layout/layout.ui.tsx
@@ -1,17 +1,78 @@
-import { Outlet } from 'react-router-dom'
-import { ScrollTop } from '~shared/lib/react-router'
-import { Footer } from '~widgets/footer'
-import { Header } from '~widgets/header'
+import { Outlet } from "react-router-dom";
+import { ScrollTop } from "~shared/lib/react-router";
+import { Footer } from "~widgets/footer";
+import { Header } from "~widgets/header";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import {
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Fab,
+  TextField,
+  useScrollTrigger,
+  Zoom,
+} from "@mui/material";
+import { useState } from "react";
 
 export function GenericLayout() {
+  // const [open, setOpen] = useState(false);
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100, // Когда появится кнопка (100px вниз)
+  });
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <ScrollTop />
+      <div className="h-28 w-full m-0 flex items-center justify-evenly z-10 bg-dove fixed">
+        <span className="text-5xl bg-tundora px-6 py-2 rounded-xl text-white">
+          Время выбрать себя
+        </span>
+        <span className="text-xl rotate-6 bg-tundora px-6 py-2 rounded-xl text-white">
+          скидка 60%
+        </span>
+      </div>
       <Header />
       <Outlet />
+      <Zoom in={trigger}>
+        <Fab
+          color="default"
+          size="large"
+          onClick={handleClick}
+          sx={{ position: "fixed", bottom: 24, right: 20 }}
+        >
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </Zoom>
+      {/* <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Обратная связь</DialogTitle>
+        <DialogContent>
+          <TextField
+            fullWidth
+            label="Ваш отзыв"
+            multiline
+            rows={4}
+            variant="outlined"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Отмена</Button>
+          <Button variant="contained" color="primary">
+            Отправить
+          </Button>
+        </DialogActions>
+      </Dialog> */}
       <Footer />
     </div>
-  )
+  );
 }
 
 export function IntroLayout() {
@@ -19,5 +80,5 @@ export function IntroLayout() {
     <div className="h-screen flex items-center justify-center">
       <Outlet />
     </div>
-  )
+  );
 }
