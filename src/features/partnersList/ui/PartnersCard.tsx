d5319a9bbaaff2eb7c5936c6c2605fc4e1a62bb0
ma1abakiev <malabakievramzan@gmail.com>
2025-03-17 11:24:15 +0600

    add partners

--- src/features/partnersList/ui/PartnersCard.tsx
+++ src/features/partnersList/ui/PartnersCard.tsx
@@ -1,17 +1,19 @@
-import { Card, Typography } from "@mui/material";
-import { PartnerCard } from "./partnersList";
+import { Card, Typography } from '@mui/material'
+import { partnerTypes } from '~entities/partner'
 
-export const PartnersCard: React.FC = ({ icon, nameLogo }: PartnerCard) => {
+export const PartnersCard: React.FC = ({
+  icon,
+  name,
+}: partnerTypes.Partner) => {
   return (
     <Card className="hover:bg-[#e5e5e5] cursor-pointer r-md:px-4 r-md:py-4 py-16 px-20 bg-[#FAFAFA] r-md:rounded-lg rounded-2xl gap-2 flex items-center r-md:w-44 w-[342px]">
-      {" "}
-      {icon}
+      <img src={`${icon}`} alt="" className="w-[80px]" />
       <Typography
         className="font-medium r-md:text-lg text-2xl"
         variant="caption"
       >
-        {nameLogo}
+        {name}
       </Typography>
     </Card>
-  );
-};
+  )
+}
--- src/features/partnersList/ui/partnersList.tsx
+++ src/features/partnersList/ui/partnersList.tsx
@@ -1,65 +1,70 @@
-import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-import TelegramIcon from "@mui/icons-material/Telegram";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import AssuredWorkloadIcon from "@mui/icons-material/AssuredWorkload";
-import GoogleIcon from "@mui/icons-material/Google";
-import GTranslateIcon from "@mui/icons-material/GTranslate";
-import ExtensionIcon from "@mui/icons-material/Extension";
+// import WhatsAppIcon from '@mui/icons-material/WhatsApp'
+// import TelegramIcon from '@mui/icons-material/Telegram'
+// import YouTubeIcon from '@mui/icons-material/YouTube'
+// import AssuredWorkloadIcon from '@mui/icons-material/AssuredWorkload'
+// import GoogleIcon from '@mui/icons-material/Google'
+// import GTranslateIcon from '@mui/icons-material/GTranslate'
+// import ExtensionIcon from '@mui/icons-material/Extension'
 
-import Marquee from "react-fast-marquee";
-import { PartnersCard } from "./PartnersCard";
+import Marquee from 'react-fast-marquee'
+import { PartnersCard } from './PartnersCard'
+import { partnerTypes } from '~entities/partner'
 
-export interface PartnerCard {
-  icon: JSX.Element;
-  nameLogo: string;
-}
-
-export const PartnersList: React.FC = () => {
-  const parnerLogoList: PartnerCard[] = [
-    {
-      icon: <WhatsAppIcon className="r-md:text-4xl text-5xl text-green" />,
-      nameLogo: "WhatsApp",
-    },
-    {
-      icon: <TelegramIcon className="r-md:text-4xl text-5xl text-blue" />,
-      nameLogo: "Telegram",
-    },
-    {
-      icon: <YouTubeIcon className="r-md:text-4xl text-5xl text-red" />,
-      nameLogo: "YouTube",
-    },
-    {
-      icon: (
-        <AssuredWorkloadIcon className="r-md:text-4xl text-5xl text-[#392fc1]" />
-      ),
-      nameLogo: "Assured",
-    },
-    {
-      icon: <GoogleIcon className="r-md:text-4xl text-5xl text-blue" />,
-      nameLogo: "Google",
-    },
-    {
-      icon: <GTranslateIcon className="r-md:text-4xl text-5xl" />,
-      nameLogo: "GTranslate",
-    },
-    {
-      icon: <ExtensionIcon className="r-md:text-4xl text-5xl" />,
-      nameLogo: "Extension",
-    },
-  ];
+export const PartnersList: React.FC<{ data: partnerTypes.Partner[] }> = ({
+  data,
+}) => {
+  // const parnerLogoList: PartnerCard[] = [
+  //   {
+  //     slug: 'whatsapp',
+  //     icon: <WhatsAppIcon className="r-md:text-4xl text-5xl text-green" />,
+  //     nameLogo: 'WhatsApp',
+  //   },
+  //   {
+  //     slug: 'whatsapp',
+  //     icon: <TelegramIcon className="r-md:text-4xl text-5xl text-blue" />,
+  //     nameLogo: 'Telegram',
+  //   },
+  //   {
+  //     slug: 'whatsapp',
+  //     icon: <YouTubeIcon className="r-md:text-4xl text-5xl text-red" />,
+  //     nameLogo: 'YouTube',
+  //   },
+  //   {
+  //     slug: 'whatsapp',
+  //     icon: (
+  //       <AssuredWorkloadIcon className="r-md:text-4xl text-5xl text-[#392fc1]" />
+  //     ),
+  //     nameLogo: 'Assured',
+  //   },
+  //   {
+  //     slug: 'whatsapp',
+  //     icon: <GoogleIcon className="r-md:text-4xl text-5xl text-blue" />,
+  //     nameLogo: 'Google',
+  //   },
+  //   {
+  //     slug: 'whatsapp',
+  //     icon: <GTranslateIcon className="r-md:text-4xl text-5xl" />,
+  //     nameLogo: 'GTranslate',
+  //   },
+  //   {
+  //     slug: 'whatsapp',
+  //     icon: <ExtensionIcon className="r-md:text-4xl text-5xl" />,
+  //     nameLogo: 'Extension',
+  //   },
+  // ]
 
   return (
     <Marquee
       direction="left"
       speed={100}
       gradient={true}
-      className="r-md:mb-20 mb-[150px] r-md:h-20 h-[200px] w-full"
+      className="r-md:mb-20 mb-[100px] r-md:h-20 h-[200px] w-full"
     >
       <div className="flex r-md:gap-4 r-md:mr-4 gap-6 mr-6">
-        {parnerLogoList.map((partnerCard: PartnerCard, index: number) => (
+        {data.map((partnerCard: partnerTypes.Partner, index: number) => (
           <PartnersCard key={index} {...partnerCard} />
         ))}
       </div>
     </Marquee>
-  );
-};
+  )
+}
